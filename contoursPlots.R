c129a8908f784353fec1118246ccd89eb338fbbd
zzawadz <zawadzkizygmunt@gmail.com>
2014-10-13 22:43:59 +0200

    Update.

--- contoursPlots.R
+++ contoursPlots.R
@@ -29,3 +29,30 @@ data(inf.mort,maesles.imm)
 data1990=na.omit(cbind(inf.mort[,1],maesles.imm[,1]))
 depthContour(data1990, method = "Mahalanobis", points = TRUE)
 dev.off()
+
+
+
+# borrowed from https://github.com/hadley/bigvis/blob/master/R/challenge.r
+rchallenge <- function(n) {
+  nt <- rbinom(1, n, 1 / 3)
+  ngamma <- n - nt
+  
+  spike <- 2 * rt(nt, df = 2) + 15
+  spike[spike < 0] <- 0
+  
+  slope <- rgamma(ngamma, 2, 1/3)
+  
+  c(spike, slope)
+}
+
+
+
+pdfFnc("t1")
+set.seed(123)
+x = cbind(rchallenge(120),rchallenge(120))
+depthContour(x, method = "Tukey", points = TRUE)
+dev.off()
+
+pdfFnc("t2")
+depthPersp(x, method = "Tukey")
+dev.off()
